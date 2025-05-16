6688bd93eb4a9cb9fee1eb18ca7dd725bfc72164
Joe Letner <jrletner@gmail.com>
2025-05-16 11:14:00 -0500

    Remove unnecessary blank line in authenticate_request method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::API
   def authenticate_request
     header = request.headers["Authorization"]
     header = header.split(" ").last if header
-
     begin
       decoded = JWT.decode(header, Rails.application.secret_key_base).first
       @current_user = User.find(decoded["user_id"])
