14d5519eff9b6081989f5c881a400b8a58320477
Joe Letner <jrletner@gmail.com>
2025-05-16 11:47:20 -0500

    Add authenticate_request before_action to UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,15 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [ :show, :update, :delete, :posts_index ]
+  before_action :authenticate_request, only: [ :update, :delete, :show, :create ]
   def index
     users = User.all
     render json: users, status: 200
   end
+
   def show
     render json: @user, status: 200
   end
+
   def create
     user = User.new(user_params)
     if user.save
@@ -15,6 +18,7 @@ class UsersController < ApplicationController
       render json: user.errors, status: :unprocessable_entity
     end
   end
+
   def update
     if @user.update(user_params)
       render json: @user, status: :ok
@@ -22,6 +26,7 @@ class UsersController < ApplicationController
       render json: @user.errors, status: :unprocessable_entity
     end
   end
+
   def delete
     if @user.destroy
       render json: @user, status: :ok
@@ -29,14 +34,18 @@ class UsersController < ApplicationController
       render json: @user.errors, status: :unprocessable_entity
     end
   end
+
   def posts_index
     user_posts = @user.posts
     render json: user_posts, status: :ok
   end
+
   private
+
   def set_user
     @user = User.find(params[:id])
   end
+
   def user_params
     params.permit(:username, :email, :first_name, :last_name, :password, :password_confirmation)
   end
