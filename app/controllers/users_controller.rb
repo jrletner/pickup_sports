d7b0b8e7ebf4bf5fe6f87dbd1353eb5e1c5f6357
Joe Letner <jrletner@gmail.com>
2025-05-16 11:27:15 -0500

    Remove unnecessary blank lines in UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,9 @@ class UsersController < ApplicationController
     users = User.all
     render json: users, status: 200
   end
-
   def show
     render json: @user, status: 200
   end
-
   def create
     user = User.new(user_params)
     if user.save
@@ -17,7 +15,6 @@ class UsersController < ApplicationController
       render json: user.errors, status: :unprocessable_entity
     end
   end
-
   def update
     if @user.update(user_params)
       render json: @user, status: :ok
@@ -25,7 +22,6 @@ class UsersController < ApplicationController
       render json: @user.errors, status: :unprocessable_entity
     end
   end
-
   def delete
     if @user.destroy
       render json: @user, status: :ok
@@ -33,17 +29,14 @@ class UsersController < ApplicationController
       render json: @user.errors, status: :unprocessable_entity
     end
   end
-
   def posts_index
     user_posts = @user.posts
     render json: user_posts, status: :ok
   end
-
   private
   def set_user
     @user = User.find(params[:id])
   end
-
   def user_params
     params.permit(:username, :email, :first_name, :last_name, :password, :password_confirmation)
   end
